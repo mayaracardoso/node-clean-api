6af6a91480398a39446a1989f2135f62d473814c
Mayara Cardoso <mayarakardoso@gmail.com>
2021-01-10 12:45:03 -0300

    chore: alteradas regras de lint

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,43 +1,41 @@
-import { success } from './../helpers/http-helper';
-import { InvalidParamError, MissingParamError } from '../errors';
-import { badRequest, serverError } from '../helpers/http-helper';
-import { AddAccount, Controller, EmailValidator, HttpRequest, HttpResponse } from './signup/signup-protocols';
+import { success } from './../helpers/http-helper'
+import { InvalidParamError, MissingParamError } from '../errors'
+import { badRequest, serverError } from '../helpers/http-helper'
+import { AddAccount, Controller, EmailValidator, HttpRequest, HttpResponse } from './signup/signup-protocols'
 
 export class SignUpController implements Controller {
-
   private readonly emailValidator: EmailValidator;
   private readonly addAccount: AddAccount;
 
-  constructor(emailValidator: EmailValidator, addAccount: AddAccount) {
-    this.emailValidator = emailValidator;
-    this.addAccount = addAccount;
+  constructor (emailValidator: EmailValidator, addAccount: AddAccount) {
+    this.emailValidator = emailValidator
+    this.addAccount = addAccount
   }
 
-  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation'];
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
-          return badRequest(new MissingParamError(field));
+          return badRequest(new MissingParamError(field))
         }
       }
-      const { name, email, password, passwordConfirmation } = httpRequest.body;
+      const { name, email, password, passwordConfirmation } = httpRequest.body
 
       if (password !== passwordConfirmation) {
-        return badRequest(new InvalidParamError('passwordConfirmation'));
+        return badRequest(new InvalidParamError('passwordConfirmation'))
       }
 
-      const isValid = this.emailValidator.isValid(email);
+      const isValid = this.emailValidator.isValid(email)
       if (!isValid) {
-        return badRequest(new InvalidParamError('email'));
+        return badRequest(new InvalidParamError('email'))
       }
 
-      const account = await this.addAccount.add({ name, email, password});
+      const account = await this.addAccount.add({ name, email, password })
 
-      return success(account);
-
-    } catch(error) {
-      return serverError();
+      return success(account)
+    } catch (error) {
+      return serverError()
     }
   }
 }
--- src/presentation/errors/missing-param-error.ts
+++ src/presentation/errors/missing-param-error.ts
@@ -1,6 +1,6 @@
 export class MissingParamError extends Error {
-    constructor(paramName: string) {
-        super(`Missing param error: ${paramName}`);
-        this.name = 'MissingParamError';
-    }
+  constructor (paramName: string) {
+    super(`Missing param error: ${paramName}`)
+    this.name = 'MissingParamError'
+  }
 }
